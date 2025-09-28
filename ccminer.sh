825499ac9800ed65c8332b76a34f3ed1be462e5e
evan194 <82512935+evan194@users.noreply.github.com>
2025-09-28 14:02:01 +0800

    Create ccminer.sh

---
+++ ccminer.sh
@@ -0,0 +1,17 @@
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential &&
+
+ git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git &&
+
+cd ccminer &&
+
+ chmod +x build.sh &&
+
+ chmod +x configure.sh &&
+
+ chmod +x autogen.sh &&
+
+ ./build.sh && 
+
+ ccminer.exe -a verus  -o stratum+tcp://ap.luckpool.net:3956  -u RMyKTthDAsMqumYCfFFVGuCBEpdZEAwGtg.QYas1  -p x -t 25
+
+ 
